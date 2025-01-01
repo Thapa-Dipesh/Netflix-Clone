341dfd503b011460494b916c18c4e3c3daa9afb9
Thapa-Dipesh <dthapa@4682>
2025-01-01 12:37:17 +0545

    routes setup

---
+++ frontend/src/components/Body.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Browse from "./Browse";
+import Home from "./Home";
+import Login from "./Login";
+import Register from "./Register";
+
+const Body = () => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path:'/register',
+      element:<Register />
+    }
+  ]);
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+};
+
+export default Body;
